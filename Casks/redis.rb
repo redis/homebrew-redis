6553b50e3456cbef25142e4c972c7f9e8400d54e
adamiBs <adam.ben-shmuel@redis.com>
2025-01-21 14:19:52 +0200

    CE 8.0 Milestone 03.
    Update CE 8.0 M03 Casks (#3)
    
    * Test CE 8.0 M03
    * Update packages url
    * Update SHAs

---
+++ Casks/redis.rb
@@ -0,0 +1,99 @@
+cask "redis" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "8.0-m03"
+  sha256 arm: "b8c4432ca2f8609165db676898944a61635dc9a38a94149309d66387e851d38d",
+         intel: "fdc3ef3e197ffb95b60bf54233fb19304b225a816477e8bd17dccbb66509b88a"
+
+  url "https://packages.redis.io/homebrew/redis-ce-#{version}-#{arch}.zip"
+  name "Redis Community Edition - Pre-Release"
+  desc "THIS IS A PRE-RELEASE VERSION!! Redis is an in-memory database that persists on disk. The data model is key-value, but many different kind of values are supported: Strings, Lists, Sets, Sorted Sets, Hashes, Streams, HyperLogLogs, Bitmaps."
+  homepage "https://redis.io/"
+
+  depends_on macos: ">= :ventura"
+
+  depends_on formula: "openssl@3"
+  depends_on formula: "libomp"
+  depends_on formula: "llvm@18"
+  
+  conflicts_with formula: "redis-rc"
+
+  binaries = %w[
+    redis-cli
+    redis-benchmark
+    redis-check-aof
+    redis-check-rdb
+    redis-sentinel
+    redis-server
+  ]
+  
+  postflight do
+    basepath = HOMEBREW_PREFIX.to_s
+    caskbase = "#{caskroom_path}/#{version}"
+    confdir = "#{basepath}/etc"
+    moduledir = "#{basepath}/lib/redis/modules"
+
+    FileUtils.mkdir_p(confdir)
+    FileUtils.mkdir_p(moduledir)
+
+    # Replace <HOMEBREW_PREFIX> with the actual value
+    src = "#{caskbase}/etc/redis.conf"
+    conffile = "#{confdir}/redis.conf"
+    FileUtils.cp(src, conffile) unless File.exist?(conffile)
+    text = File.read(conffile)
+    new_contents = text.gsub("<HOMEBREW_PREFIX>", basepath)
+    File.open(conffile, "w") { |file| file.puts new_contents }
+
+    # link binaries
+    binaries.each do |item|
+      src = "#{caskbase}/bin/#{item}"
+      dest = "#{basepath}/bin/#{item}"
+      FileUtils.ln_sf(src, dest)
+    end
+
+    # link modules
+    Dir["#{caskbase}/lib/redis/modules/*.so"].each do |item|
+      module_name = File.basename(item)
+      dest = "#{moduledir}/#{module_name}"
+      File.symlink(item, dest) unless File.exist?(dest)
+    end
+  end
+
+  uninstall_postflight do
+    basepath = HOMEBREW_PREFIX.to_s
+
+    # Remove binary symlinks
+    binaries.each do |item|
+      dest = "#{basepath}/bin/#{item}"
+      File.delete(dest) if File.symlink?(dest) && File.exist?(dest)
+    end
+
+    # Remove module symlinks
+    moduledir = "#{basepath}/lib/redis/modules"
+    Dir["#{moduledir}/*.so"].each do |item|
+      module_name = File.basename(item)
+      dest = "#{moduledir}/#{module_name}"
+      File.delete(dest)
+    end
+
+    # Clean up empty directories
+    FileUtils.rm_rf(moduledir) if Dir.empty?(moduledir)
+    FileUtils.rm_rf("#{basepath}/lib/redis") if Dir.empty?("#{basepath}/lib/redis")
+  end
+
+  caveats <<~EOS
+    🚨THIS IS A PRE-RELEASE VERSION!! BREAKING CHANGES MAY OCCUR WITHOUT NOTICE!!🚨
+    Redis Community Edition has been successfully installed!
+
+    The default configuration file has been copied to:
+      #{HOMEBREW_PREFIX}/etc/redis.conf
+
+    To customize Redis, edit this file as needed and restart Redis to apply changes.
+
+    If you want to run Redis as a service, use:
+      redis-server #{HOMEBREW_PREFIX}/etc/redis.conf
+
+    To stop the service:
+      redis-cli shutdown
+  EOS
+end
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# homebrew-redis
-Redis on the Homebrew package manager
+# Redis Community Edition Homebrew Cask
+
+This repository provides a Homebrew Cask for installing the Redis Community Edition binary distribution.
+
+## Installation
+
+To install Redis Community Edition using Homebrew Cask, run the following command:
+
+```bash
+brew tap redis/redis
+brew install --cask redis
+```
+
+For pre-release versions, you can use the following command. Note that this will install the latest pre-release version and override the stable version:
+
+```bash
+brew tap redis/redis
+brew install --cask redis-rc
+```
+
+## Usage
+
+After installation, you can start Redis using the following command:
+
+```bash
+redis-server
+```
+
+If you want to start Redis in the background, you can use the following command:
+
+```bash
+redis-server $(homebrew --prefix)/etc/redis.conf
+```
+
+## Contributing
+
+If you would like to contribute to this project, please fork the repository and submit a pull request.
