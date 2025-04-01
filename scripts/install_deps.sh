1b9007be0e69ee0250daf9a1862841a76fdfa525
adamiBs <adam.ben-shmuel@redis.com>
2025-04-01 11:34:00 +0300

    RED-155190 Build fixes (#15)

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -6,10 +6,14 @@ brew install coreutils
 brew install make
 brew install openssl
 brew install llvm@18
-brew install cmake@3.31.6
 brew install gnu-sed
 brew install automake
 brew install libtool
+
+curl -L -o cmake-3.31.6.tar.gz https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6.tar.gz
+tar -xzf cmake-3.31.6.tar.gz
+cd cmake-3.31.6
+./bootstrap && make && sudo make install
 
 RUST_INSTALLER=rust-1.80.1-$(if [ "$(uname -m)" = "arm64" ]; then echo "aarch64"; else echo "x86_64"; fi)-apple-darwin
 echo "Downloading and installing Rust standalone installer: ${RUST_INSTALLER}"
