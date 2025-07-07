d9fa7c8404b9d03ae8c6d75da4661d10750ed9c2
deborahjalfont <80630578+deborahjalfont@users.noreply.github.com>
2025-07-07 19:43:39 +0300

    RED-158292 add unstable build packaging of homebrew distribution (#21)

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,25 +2,26 @@
 
 export HOMEBREW_PREFIX="$(brew --prefix)"
 export BUILD_WITH_MODULES=yes
+export MODULE_VERSION=master
 export BUILD_TLS=yes
 export DISABLE_WERRORS=yes
 PATH="$HOMEBREW_PREFIX/opt/libtool/libexec/gnubin:$HOMEBREW_PREFIX/opt/llvm@18/bin:$HOMEBREW_PREFIX/opt/make/libexec/gnubin:$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin:$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH" # Override macOS defaults.
 export LDFLAGS="-L$HOMEBREW_PREFIX/opt/llvm@18/lib"
 export CPPFLAGS="-I$HOMEBREW_PREFIX/opt/llvm@18/include"
 
-# Check if Redis version is provided as an argument
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 <redis_version>"
-  exit 1
-fi
+curl -L "https://github.com/redis/redis/archive/refs/heads/unstable.tar.gz" -o redis-unstable.tar.gz
+tar xzf redis-unstable.tar.gz
 
-REDIS_VERSION="$1"
-
-curl -L "https://github.com/redis/redis/archive/refs/tags/$REDIS_VERSION.tar.gz" -o redis.tar.gz
-tar xzf redis.tar.gz
+# Update module versions to use master branch
+for module in redisbloom redisearch redistimeseries redisjson; do
+  if [ -f "redis-unstable/modules/${module}/Makefile" ]; then
+    sed -i 's/MODULE_VERSION = .*/MODULE_VERSION = master/' "redis-unstable/modules/${module}/Makefile"
+    echo "Updated MODULE_VERSION to master for ${module}"
+  fi
+done
 
 mkdir -p build_dir/etc
-make -C redis-$REDIS_VERSION -j "$(nproc)" all OS=macos
-make -C redis-$REDIS_VERSION install PREFIX=$(pwd)/build_dir OS=macos
+make -C redis-unstable -j "$(nproc)" all OS=macos
+make -C redis-unstable install PREFIX=$(pwd)/build_dir OS=macos
 cp ./configs/redis.conf build_dir/etc/redis.conf
-(cd build_dir && zip -r ../redis-ce-$REDIS_VERSION-$(uname -m).zip .)
+(cd build_dir && zip -r ../unsigned-redis-ce-unstable-$(uname -m).zip .)
