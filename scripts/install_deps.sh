4c7481fd426c3097cb45951ee5ce45dd1f351225
Daria Guy <daria.guy@redis.com>
2026-06-28 10:28:28 +0300

    Add uv dependency for rediseach build

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -10,6 +10,7 @@ brew install llvm@18
 brew install gnu-sed
 brew install automake
 brew install libtool
+brew install uv
 
 # Ensure Homebrew's cmake does not interfere
 brew uninstall --ignore-dependencies cmake || true
